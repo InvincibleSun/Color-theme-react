d361ecde2a0135b26df52ed33919f4f0a926e15e
InvincibleSun <103495967+InvincibleSun@users.noreply.github.com>
2022-12-01 00:00:19 +0400

    Added content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from "react";
 import "./App.css";
+import Navbar from "./component/Navbar";
+import Main from "./component/Main";
 
 function App() {
-  return <div className="App"></div>;
+  const [mode, setMode] = React.useEffect(false);
+  function toggleMode() {
+    setMode((prevMode) => !prevMode);
+  }
+
+  return (
+    <div className="container">
+      <Navbar darkMode={mode} toggleDarkMode={toggleMode} />
+      <Main arkMode={mode} />
+    </div>
+  );
 }
 
 export default App;
